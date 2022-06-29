a5d9076c668357bbee9d5215a22675e3a83fe99e
srijan <srijandasbiswas7540@gmail.com>
2022-06-30 02:00:18 +0530

    added build version

---
+++ build/precache-manifest.2838f610550b80dcd85afd6c2effbcfe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32a1c68c8e7b39175b43596fa5e43ee7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d4ad33425482638a6894",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "a1ab830003a33e7d59fe",
+    "url": "/static/css/main.2fb7ba53.chunk.css"
+  },
+  {
+    "revision": "d4ad33425482638a6894",
+    "url": "/static/js/2.9cbe4ee7.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.9cbe4ee7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1ab830003a33e7d59fe",
+    "url": "/static/js/main.acdcfb6b.chunk.js"
+  },
+  {
+    "revision": "7daa7f2e6a3851b61941",
+    "url": "/static/js/runtime-main.fa560a86.js"
+  }
+]);
